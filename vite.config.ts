ff142f504584f8369c7d929b42ba4b93ae22f9a6
guidssantos <guilhermedssantos1@gmail.com>
2024-05-27 18:59:28 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,4 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      components: `${__dirname}/src/components`,
-      styles: `${__dirname}/src/styles`,
-      assets: `${__dirname}/src/assets`,
-      utils: `${__dirname}/src/utils`,
-    },
-  },
-  define: {
-    "proces.env": process.env,
-    global: "window",
-  },
 });
